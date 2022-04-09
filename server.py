a104b3c7b2cde3570209e7fe1e16111fb10db813
Josh Pearson <36744690+baconeta@users.noreply.github.com>
2022-04-10 01:03:23 +0300

    add TODOs to help guide next steps

--- server.py
+++ server.py
@@ -31,23 +31,26 @@ print('The server is running.')
 
 # Fetch the requested file, and send the contents back to the client in an HTTP response.
 def generate_html_head(filename):
+    # TODO make sure this only uses the necessary header data
     if filename == "portfolio":
         return "<script data-main='scripts/main' src='scripts/require.js'></script> <script " \
                "src='scripts/main.js'></script> <script " \
                "src='https://ajax.googleapis.com/ajax/libs/jquery/3.4.1/jquery.min.js'></script> "
+    # TODO handle research page
     return ""
 
 
 def generate_html_body(filename):
+    # TODO make this populate the table correctly from the JSON file or else create a blank table (do both together?)
     if filename == "portfolio":
         return "<h1>Josh's Investment Portfolio</h1><button onclick='constructTable('#table')'> click here </button> " \
-               "<br><br> <table align='center' id='table' border='1'> </table> <br> <!--action='/action_page.php' " \
-               "TODO add action here--> <!--TODO add alignment--> <form method='post' target='_self'> <label " \
-               "for='stock-symbol'>Stock Symbol:</label> <input id='stock-symbol' name='stock-symbol' required " \
-               "type='text'><br><br> <label for='quantity'>Quantity:</label> <input id='quantity' name='quantity' " \
-               "required step=any type='number'><br><br> <label for='price'>Share price:</label> <input id='price' " \
-               "name='price' required step=0.01 type='number'><br><br> <button type='submit' " \
-               "formmethod='post'>Update</button> </form> "
+               "<br><br> <table align='center' id='table' border='1'> </table> <br> <form method='post' " \
+               "target='_self'> <label for='stock-symbol'>Stock Symbol:</label> <input id='stock-symbol' " \
+               "name='stock-symbol' required type='text'><br><br> <label for='quantity'>Quantity:</label> <input " \
+               "id='quantity' name='quantity' required step=any type='number'><br><br> <label for='price'>Share " \
+               "price:</label> <input id='price' name='price' required step=0.01 type='number'><br><br> <button " \
+               "type='submit' formmethod='post'>Update</button> </form> "
+    # TODO handle research page
     return ""
 
 
@@ -123,6 +126,7 @@ def generate_requested_page(parsed_headers):
         return make_file("research")
     else:
         return make_file("404")
+    # TODO possibly add some way to handle extra embedded scripts from inside the html if required
 
 
 def serve_site(parsed_headers):
